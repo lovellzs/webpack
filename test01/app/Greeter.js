b979a464d7871e8331b8e8fae3db34910afd25bc
shizhe <apple@shizhe.local>
2017-11-14 18:43:25 +0800

    Webpack  初探

---
+++ test01/app/Greeter.js
@@ -0,0 +1,9 @@
+/**
+ * Created by apple on 2017/11/14.
+ */
+// Greeter.js
+module.exports = function() {
+    var greet = document.createElement('div');
+    greet.textContent = "Hi there and greetings!";
+    return greet;
+};
---
+++ test01/app/main.js
@@ -0,0 +1,6 @@
+/**
+ * Created by apple on 2017/11/14.
+ */
+//main.js
+const greeter = require('./Greeter.js');
+document.querySelector("#root").appendChild(greeter());
